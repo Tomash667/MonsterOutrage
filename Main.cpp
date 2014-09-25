e49940cf6bfadf95588db45ba0bce2174f9d808b
Tomash <tomash4my@gmail.com>
2014-09-25 18:00:28 +0200

    moving takes time, animation, better keyboard handlign

--- Main.cpp
+++ Main.cpp
@@ -6,6 +6,7 @@
 #include <ctime>
 
 typedef const char* cstring;
+typedef unsigned char byte;
 typedef unsigned int uint;
 
 cstring Format(cstring str, ...)
@@ -32,13 +33,68 @@ cstring Format(cstring str, ...)
 const int VERSION = 0;
 const int MAP_SIZE = 15;
 
+enum DIR
+{
+	DIR_S,
+	DIR_SW,
+	DIR_W,
+	DIR_NW,
+	DIR_N,
+	DIR_NE,
+	DIR_E,
+	DIR_SE,
+	DIR_INVALID
+};
+
+struct INT2
+{
+	int x, y;
+
+	INT2() {}
+	INT2(int x, int y) : x(x), y(y) {}
+
+	inline INT2 operator + (const INT2& pt) const
+	{
+		return INT2(x+pt.x, y+pt.y);
+	}
+};
+
+INT2 dir_change[] = {
+	INT2(0,1),
+	INT2(-1,1),
+	INT2(-1,0),
+	INT2(-1,-1),
+	INT2(0,-1),
+	INT2(1,-1),
+	INT2(1,0),
+	INT2(1,1),
+	INT2(0,0)
+};
+
+enum InputState
+{
+	IS_UP,			// 00
+	IS_RELEASED,	// 01
+	IS_DOWN,		// 10
+	IS_PRESSED		// 11
+};
+
 SDL_Window* window;
 SDL_Renderer* renderer;
+byte keystate[SDL_NUM_SCANCODES];
+
+inline bool KeyDown(int key)
+{
+	return (keystate[key] & IS_DOWN) != 0;
+}
 
 SDL_Texture* tTiles, *tUnit;
-int x, y;
 bool mapa[MAP_SIZE*MAP_SIZE];
 SDL_Rect tiles[2];
+INT2 pos, new_pos;
+DIR dir;
+bool moving;
+float move_progress;
 
 void InitSDL()
 {
@@ -51,7 +107,7 @@ void InitSDL()
 	if(!window)
 		throw Format("ERROR: Failed to create window (%d).", SDL_GetError());
 
-	renderer = SDL_CreateRenderer(window, 0, SDL_RENDERER_ACCELERATED);
+	renderer = SDL_CreateRenderer(window, 0, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 	if(!renderer)
 		throw Format("ERROR: Failed to create renderer (%d).", SDL_GetError());
 
@@ -134,11 +190,60 @@ void Draw()
 	}
 	
 	// unit
-	r.x = x*32;
-	r.y = y*32;
+	if(moving)
+	{
+		r.x = pos.x*32+int(move_progress*32*dir_change[dir].x);
+		r.y = pos.y*32+int(move_progress*32*dir_change[dir].y);
+	}
+	else
+	{
+		r.x = pos.x*32;
+		r.y = pos.y*32;
+	}
 	SDL_RenderCopy(renderer, tUnit, NULL, &r);
 
 	SDL_RenderPresent(renderer);
+}
+
+void Update(float dt)
+{
+	if(moving)
+	{
+		move_progress += dt*5;
+		if(move_progress >= 1.f)
+		{
+			moving = false;
+			pos = new_pos;
+		}
+	}
+	else
+	{
+		DIR new_dir = DIR_INVALID;
+		if((KeyDown(SDL_SCANCODE_LEFT) || KeyDown(SDL_SCANCODE_KP_4)) && pos.x > 0 && !mapa[pos.x-1+pos.y*MAP_SIZE])
+			new_dir = DIR_W;
+		else if((KeyDown(SDL_SCANCODE_RIGHT) || KeyDown(SDL_SCANCODE_KP_6)) && pos.x < MAP_SIZE-1 && !mapa[pos.x+1+pos.y*MAP_SIZE])
+			new_dir = DIR_E;
+		else if((KeyDown(SDL_SCANCODE_UP) || KeyDown(SDL_SCANCODE_KP_8)) && pos.y > 0 && !mapa[pos.x+(pos.y-1)*MAP_SIZE])
+			new_dir = DIR_N;
+		else if((KeyDown(SDL_SCANCODE_DOWN) || KeyDown(SDL_SCANCODE_KP_2)) && pos.y < MAP_SIZE-1 && !mapa[pos.x+(pos.y+1)*MAP_SIZE])
+			new_dir = DIR_S;
+		else if(KeyDown(SDL_SCANCODE_KP_1) && pos.x > 0 && pos.y < MAP_SIZE-1 && !mapa[pos.x-1+(pos.y+1)*MAP_SIZE])
+			new_dir = DIR_SW;
+		else if(KeyDown(SDL_SCANCODE_KP_3) && pos.x < MAP_SIZE-1 && pos.y < MAP_SIZE-1 && !mapa[pos.x+1+(pos.y+1)*MAP_SIZE])
+			new_dir = DIR_SE;
+		else if(KeyDown(SDL_SCANCODE_KP_7) && pos.x > 0 && pos.y > 0 && !mapa[pos.x-1+(pos.y-1)*MAP_SIZE])
+			new_dir = DIR_NW;
+		else if(KeyDown(SDL_SCANCODE_KP_9) && pos.x < MAP_SIZE-1 && pos.y > 0 && !mapa[pos.x+1+(pos.y-1)*MAP_SIZE])
+			new_dir = DIR_NE;
+
+		if(new_dir != DIR_INVALID)
+		{
+			dir = new_dir;
+			moving = true;
+			move_progress = 0.f;
+			new_pos = pos + dir_change[dir];
+		}
+	}
 }
 
 int main(int argc, char *argv[])
@@ -152,6 +257,7 @@ int main(int argc, char *argv[])
 		InitGame();
 
 		bool quit = false;
+		uint ticks = SDL_GetTicks();
 
 		while(!quit)
 		{
@@ -162,64 +268,34 @@ int main(int argc, char *argv[])
 					quit = true;
 				else if(e.type == SDL_KEYDOWN)
 				{
-					switch(e.key.keysym.sym)
+					if(e.key.state == SDL_PRESSED)
 					{
-					case SDLK_UP:
-					case SDLK_KP_8:
-						if(y > 0 && !mapa[x+(y-1)*MAP_SIZE])
-							--y;
-						break;
-					case SDLK_DOWN:
-					case SDLK_KP_2:
-						if(y < MAP_SIZE-1 && !mapa[x+(y+1)*MAP_SIZE])
-							++y;
-						break;
-					case SDLK_LEFT:
-					case SDLK_KP_4:
-						if(x > 0 && !mapa[x-1+y*MAP_SIZE])
-							--x;
-						break;
-					case SDLK_RIGHT:
-					case SDLK_KP_6:
-						if(x < MAP_SIZE-1 && !mapa[x+1+y*MAP_SIZE])
-							++x;
-						break;
-					case SDLK_ESCAPE:
-						quit = true;
-						break;
-					case SDLK_KP_1:
-						if(x > 0 && y < MAP_SIZE-1 && !mapa[x-1+(y+1)*MAP_SIZE])
-						{
-							--x;
-							++y;
-						}
-						break;
-					case SDLK_KP_3:
-						if(x < MAP_SIZE-1 && y < MAP_SIZE-1 && !mapa[x+1+(y+1)*MAP_SIZE])
-						{
-							++x;
-							++y;
-						}
-						break;
-					case SDLK_KP_7:
-						if(x > 0 && y > 0 && !mapa[x-1+(y-1)*MAP_SIZE])
-						{
-							--x;
-							--y;
-						}
-						break;
-					case SDLK_KP_9:
-						if(x < MAP_SIZE-1 && y > 0 && !mapa[x+1+(y-1)*MAP_SIZE])
-						{
-							++x;
-							--y;
-						}
-						break;
+						if(keystate[e.key.keysym.scancode] <= IS_RELEASED)
+							keystate[e.key.keysym.scancode] = IS_PRESSED;
+					}
+				}
+				else if(e.type == SDL_KEYUP)
+				{
+					if(e.key.state == SDL_RELEASED)
+					{
+						if(keystate[e.key.keysym.scancode] >= IS_DOWN)
+							keystate[e.key.keysym.scancode] = IS_RELEASED;
 					}
 				}
 			}
 
+			uint new_ticks = SDL_GetTicks();
+			float dt = float(new_ticks - ticks)/1000.f;
+			ticks = new_ticks;
+
 			Draw();
+			Update(dt);
+
+			for(uint i = 0; i < SDL_NUM_SCANCODES; ++i)
+			{
+				if(keystate[i] & 1)
+					--keystate[i];
+			}
 		}
 
 		CleanMedia();
@@ -234,6 +310,3 @@ int main(int argc, char *argv[])
 
 	return 0;
 }
-
-// http://lazyfoo.net/tutorials/SDL/index.php
-// TODO: animacja ruchu
